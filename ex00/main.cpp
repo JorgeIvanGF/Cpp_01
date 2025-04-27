565f70801922aea2a4a281c4e4b568c77d7f19aa
JorgeIvanGF <jorgeivangf@gmail.com>
2025-04-27 23:31:50 +0200

    CHORE(main): Implementation of the main file, organized by timeline.

---
+++ ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 22:30:04 by jorgutie          #+#    #+#             */
+/*   Updated: 2025/04/27 23:27:28 by jorgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Zombie.hpp"
+
+// Declaration of the functions to tell the compiler that these functions already exist.
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+// The main function
+int main()
+{
+	// Created on Heap
+	std::cout << "\n* A Zombie is being created on the " << YELLOW  "Heap" << RESET << std::endl;
+	Zombie *zombie_heap = newZombie("zombie_heap");  
+	zombie_heap->announce();
+	
+	// Creation on Stack
+	std::cout << "\n* A Zombie is being created on the " << YELLOW "Stack" << RESET << std::endl;
+	randomChump("zombie_stack");
+	
+
+	// Delete manually the Zombie Heap
+	std::cout  << "\nThe " << MAGENTA "zombie_heap" << RESET <<" is being deleted manually..." << std::endl;
+    delete zombie_heap; // Delete heap Zombie to avoid memory leaks!
+	
+	
+	// If a Zombie still exists the Destructor will delete it automatically
+	std::cout << "\nProgram finished. Zombies (if any) will be destroyed automatically by " << RED << "Destructor" << std::endl;
+
+	return (0);
+}
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,11 +6,13 @@
 /*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:00:49 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/04/27 20:42:53 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/04/27 22:33:59 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+
 
 // Creation of a Zombie on the STACK and announce itself
 void randomChump( std::string name )
