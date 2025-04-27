cfa5a1b4d4a41c71fa5df8e315e99e086b6ffea5
JorgeIvanGF <jorgeivangf@gmail.com>
2025-04-27 22:29:01 +0200

    BUILT(Zombies): Creation of instances of Zombie (on the Stack and on the Heap) and calling the announce function

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,15 @@
 /*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:12:30 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/04/27 18:12:31 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/04/27 20:31:26 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+// Creation of a Zombie in the HEAP.
+Zombie *newZombie(std::string name)
+{
+	Zombie* new_zombie = new Zombie(name); // allocate Zombie on the heap (keyword "NEW")
+	return new_zombie; // return the pointer
+}
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,7 +6,16 @@
 /*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:00:49 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/04/27 18:00:50 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/04/27 20:42:53 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+// Creation of a Zombie on the STACK and announce itself
+void randomChump( std::string name )
+{
+	Zombie random_zombie(name); // create a zombie on STACK
+	random_zombie.announce();  // announce itself
+	 
+}
