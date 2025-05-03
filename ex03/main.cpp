d64ce39647e8c8ea7cb95dd1a27b6b58e96a1aa2
JorgeIvanGF <jorgeivangf@gmail.com>
2025-05-03 14:37:35 +0200

    CHORE(main): main function added

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,10 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jorgutie <jorgutie@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:00:06 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/05/02 16:00:07 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/05/03 14:31:48 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		std::cout << "\n" ;
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.attack();
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
