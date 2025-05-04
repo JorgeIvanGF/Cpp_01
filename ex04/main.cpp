3087706ed52d5a60643e45dbf114e30acfc8dd22
JorgeIvanGF_WSL <jorgeivangf@gmail.com>
2025-05-04 16:06:46 +0200

    CHORE(replace/write): Functions for replacement and writing the output created.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 14:47:59 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/05/04 14:59:57 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/05/04 15:50:26 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,82 +33,100 @@ bool openFile(const std::string &filename, std::ifstream &input)
 	return true;
 }
 
-// 2) Read the entire contents of 'input' into 'content'
-bool readFile(std::ifstream& input, std::string& content) {
-    std::string line;
-    while (std::getline(input, line)) {
-        content += line;
-        content += '\n';
-    }
-    if (input.bad()) {
-        std::cerr << RED
-                  << "Error: Failed while reading input file"
-                  << RESET << '\n';
-        return false;
-    }
-    input.close();
-    return true;
+// 2) Read and storage the entire content of 'input' in 'content'
+bool readFile(std::ifstream& input, std::string& content) 
+{
+	std::string line;
+	while (std::getline(input, line)) 
+	{
+		content += line;
+		content += '\n';
+	}
+	if (input.bad())
+	{
+		std::cerr << RED
+				  << "Error: Failed while reading input file"
+				  << RESET << '\n';
+		return false;
+	}
+	input.close();
+	return true;
 }
 
+// 3) Replace all occurrences of s1 with s2 in-place
+void replaceStrings(std::string& content,
+	const std::string& s1, const std::string& s2)
+{
+	size_t pos = 0;
+	while ((pos = content.find(s1, pos)) != std::string::npos)
+	{
+		content.erase(pos, s1.length()); // Remove s1 from content
+		content.insert(pos, s2); // Insert s2 in its place
+		pos += s2.length(); // move pos to the end of s2
+	}
+}
+
+// 4) Write 'content' to filename + ".replace"
+bool writeFile(const std::string& filename, const std::string& content)
+{
+    const std::string outName = filename + ".replace";
+    std::ofstream output(outName);  // direct std::string overload
+    if (!output.is_open())
+	{
+        std::cerr << RED
+                  << "Error: Could not create file "
+                  << YELLOW << outName << RESET
+                  << '\n';
+        return false;
+    }
+    output << content;
+    if (!output)
+	{
+        std::cerr << RED
+                  << "Error: Failed while writing to file "
+                  << YELLOW << outName << RESET
+                  << '\n';
+        return false;
+    }
+    output.close();
+    return true;
+}
 
 int main(int ac, char **av)
 {
 	// Check if 3 args
 	if(ac == 4)
 	{
-		// Assignments of the args __________________________________________________
+		// Assignments of the args
 		const std::string filename = av[1];
 		const std::string s1 = av[2];
 		const std::string s2 = av[3];
 
-		// S1 empty
+		// Check if S1 empty
 		if(s1.empty())
 		{
 			std::cerr << RED << "Error: s1 can't be empty" 
 					  << RESET << std::endl;
 			return 1;
 		}
-		
-		// Main Logic of  the Program:_______________________
-		
+				
 		// Open the file using input_file stream object,
-		// similar to "std::cin", but from a file not user input
+		// similar to "std::cin", but from a file not "user input"
 		std::ifstream input; 		
-		if (!openFile(filename, input)) return 1; 
+		if (!openFile(filename, input)) 
+			return 1; 
 	
 		// To read and storage the content of the file in one string
 		std::string content; 
-		if (!readFile(input, content)) return 1;
-		
-
-		// Find-Replace part_______________________________________________________
-		size_t pos = 0; // To track the position
-		while((pos = content.find(s1,pos)) != std::string::npos)
-		{
-			// Remove s1 from content
-			content.erase(pos, s1.length());
-			
-			// Insert s2 in its place
-			content.insert(pos, s2);
-
-			// move pos to the end of s2
-			pos += s2.length();
-		}
-
-		// Write to output file _____________________________________________________
-		std::string outName = filename + ".replace"; // Creates the output filename
-		
-		std::ofstream output(outName.c_str());
-		if (!output.is_open()) 
-		{
-			std::cerr << RED << "Error: Could not create file " 
-					  << YELLOW << outName << RESET << std::endl;
+		if (!readFile(input, content))
 			return 1;
-		}
 		
-		output << content;
-		output.close();
+		// The Replacement of the strings
+		replaceStrings(content, s1, s2);
 		
+		// The  Write to a new file part
+		if (!writeFile(filename, content))
+			return 1;		
 	}
 	else
 	{
@@ -116,4 +134,5 @@ int main(int ac, char **av)
 				  << RESET << std::endl;
 		return 1;
 	}
+	return 0;
 }
